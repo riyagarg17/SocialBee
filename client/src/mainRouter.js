2fe0c01fe8f1ca320507dc07f9813e55ead51897
riyagarg7d@gmail.com <riyagarg7d@gmail.com>
2019-07-18 00:32:30 +0530

    changes

--- client/src/mainRouter.js
+++ client/src/mainRouter.js
@@ -6,16 +6,17 @@ import Dashboard from './components/Dashboard/Dashboard';
 import Profile from './components/Profile/Profile';
 import Settings from './components/Profile/Settings/Settings';
 import AddPost from './components/Posts/AddPost';
+import PrivateRoute from './auth/PrivateRoute';
 
 const mainRouter=(props)=>(
     <>
         <Switch>
             <Route exact path="/" component={Home}/>
             <Route exact path="/login" component={Login}/>
-            <Route exact path="/dashboard"component={Dashboard}/>
-            <Route exact path="/user/:userId" component={Profile}/>
-            <Route exact path="/settings/:userId" component={Settings}/>
-            <Route exact path="/post/add" component={AddPost}/>
+            <PrivateRoute exact path="/dashboard"component={Dashboard}/>
+            <PrivateRoute exact path="/user/:userId" component={Profile}/>
+            <PrivateRoute exact path="/settings/:userId" component={Settings}/>
+            <PrivateRoute exact path="/post/add" component={AddPost}/>
         </Switch>
     </>
 )
