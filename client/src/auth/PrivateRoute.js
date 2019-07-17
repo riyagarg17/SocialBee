2a98de92ca694cc6ff2a04fbbfc2d33c5e4cf1db
riyagarg7d@gmail.com <riyagarg7d@gmail.com>
2019-07-18 00:41:46 +0530

    changes

--- client/src/auth/PrivateRoute.js
+++ client/src/auth/PrivateRoute.js
@@ -2,22 +2,24 @@ import React, { Component } from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { isAuthenticated } from './index';
 
-const PrivateRoute = ({ component: Component, ...rest }) => {
-
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    
     <Route
         {...rest}
-        render={props => isAuthenticated() ? (
-                <Component {...props} />) :
-                 (<Redirect
-                        to={{
-                            pathname: "/login",
-                            state: { from: props.location }
-                        }}
-                    />
-                )
+        render={props =>
+            isAuthenticated() ? (
+                <Component {...props} />
+            ) : (
+                <Redirect
+                    to={{
+                        pathname: "/login",
+                        state: { from: props.location }
+                    }}
+                />
+            )
         }
     />
-};
+);
 
 
 export default PrivateRoute;
